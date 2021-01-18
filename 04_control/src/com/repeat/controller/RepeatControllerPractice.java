65d1e8a62715998caef8d9028f6de641a2e0198a
j415301 <76389633+j415301@users.noreply.github.com>
2021-01-19 00:34:02 +0900

    Add files via upload

--- 04_control/src/com/repeat/controller/RepeatControllerPractice.java
+++ 04_control/src/com/repeat/controller/RepeatControllerPractice.java
@@ -157,5 +157,221 @@ public class RepeatControllerPractice {
 			}
 		}
 	}
-
+	
+	public void whileTest() {
+		//while문
+		
+		//1~100까지 출력해보자
+		int i = 1;
+		while (i<=100) {
+			System.out.print(i+" ");
+			i++;
+		}
+		
+		//무한반복
+//		while (true) {
+//			Scanner sc = new Scanner(System.in);
+//			System.out.println("===== 계산기 02 =====");
+//			System.out.println("몇 번의 계산을 수행하시겠습니까?");
+//			int count = sc.nextInt();
+//			for (int f=0 ; f<count ; f++) {
+//				System.out.print("수1: ");
+//				int su1 = sc.nextInt();
+//				System.out.print("연산자: ");
+//				char op = sc.next().charAt(0);
+//				System.out.print("수2: ");
+//				int su2 = sc.nextInt();
+//				switch (op) {
+//					case '+': System.out.println(su1+su2); break;
+//					case '-': System.out.println(su1-su2); break;
+//					case '*': System.out.println(su1*su2); break;
+//					case '/': System.out.println((double)su1/su2); break;
+//					default: System.out.println("잘못 입력하셨습니다.");
+//				}
+//			}
+//			System.out.println("계산기를 종료하시겠습니까? (Y/N)");
+//			char yesno = sc.next().charAt(0);
+//			if (yesno == 'Y') {
+//				break;
+//			}
+//		}
+//		
+//		int j=1;
+//		while (j==1) {
+//			Scanner sc = new Scanner(System.in);
+//			System.out.println("===== 계산기 02 =====");
+//			System.out.println("몇 번의 계산을 수행하시겠습니까?");
+//			int count = sc.nextInt();
+//			for (int f=0 ; f<count ; f++) {
+//				System.out.print("수1: ");
+//				int su1 = sc.nextInt();
+//				System.out.print("연산자: ");
+//				char op = sc.next().charAt(0);
+//				System.out.print("수2: ");
+//				int su2 = sc.nextInt();
+//				switch (op) {
+//					case '+': System.out.println(su1+su2); break;
+//					case '-': System.out.println(su1-su2); break;
+//					case '*': System.out.println(su1*su2); break;
+//					case '/': System.out.println((double)su1/su2); break;
+//					default: System.out.println("잘못 입력하셨습니다.");
+//				}
+//			}
+//			System.out.println("계산기를 종료하시겠습니까? (Y/N)");
+//			char yesno = sc.next().charAt(0);
+//			if (yesno == 'Y') {
+//				break;
+//			}
+//		}
+		System.out.println();
+		
+		//do ~ while문
+		int a = 10;
+		while (a>10) {
+			System.out.println(a);
+		}//아무것도 출력되지 않음
+		
+		do {
+			System.out.println(a);
+		} while (a>10);
+	}
+	
+	public void doubleForTest() {
+		//중첩 반복문
+		
+		for (int i=0 ; i<10 ; i++) {
+			System.out.println("외곽: "+i);
+			for (int j=0 ; j<10 ; j++) {
+				System.out.print("내곽: "+j+" ");
+			}
+			System.out.println();
+		}
+		
+		//구구단 출력하기
+		for (int i=2 ; i<10 ; i++) {
+			System.out.println("===== "+i+"단 =====");
+			for (int j=1 ; j<10 ; j++) {
+				System.out.println(i+"x"+j+" = "+i*j);
+			}
+		}
+		
+		//입력받은 값의 각 문자별 중복개수 확인
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("문자열을 입력하시오.");
+//		String voca = sc.next();
+//		int count = 0;
+//		for (int i=0 ; i<voca.length() ; i++) {
+//			for (int j=0 ; j<voca.length() ; j++) {
+//				if (voca.charAt(i)==voca.charAt(j) && i!=j) {
+//					count++;
+//				}
+//			}
+//			System.out.println(voca.charAt(i)+" 중복 횟수: "+count);
+//		}
+		
+		//별 찍기
+		//***
+		//***
+		//***
+		for (int i=0 ; i<3 ; i++) {
+			System.out.println("***");
+		}
+		System.out.println();
+		
+		//*
+		//**
+		//***
+		for (int i=0 ; i<3 ; i++) {
+			for (int j=0 ; j<=i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		//***
+		//**
+		//*
+		for (int i=0 ; i<3 ; i++) {
+			for (int j=0 ; j<3-i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void breakContinueTest() {
+		Scanner sc = new Scanner(System.in);
+//		String orderMenu = "";
+//		while (true) {
+//			System.out.println("===== 점심주문 프로그램 ====");
+//			System.out.println("1. 회\n2. 연어덮밥\n3. 감자탕\n4. 샌드위치");
+//			System.out.print("입력 : ");
+//			int cho = sc.nextInt();
+//			switch (cho) {
+//				case 1: orderMenu += "회\t"; break;
+//				case 2: orderMenu += "연어덮밥\t"; break;
+//				case 3: orderMenu += "감자탕\t"; break;
+//				case 4: orderMenu += "샌드위치\t"; break;
+//			}
+//			System.out.println("계속 주문하시겠습니까? (Y/N)");
+//			char yesno = sc.next().charAt(0);
+//			if (yesno == 'N') {
+//				System.out.println("===== 주문 내역 =====");
+//				System.out.println(orderMenu);
+//				break;
+//			}
+//		}
+//		System.out.println("[주문 프로그램 종료]");
+		
+		for (int i=0 ; i<10 ; i++) {
+			if (i%2==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		System.out.println();
+		
+		for (int i=2 ; i<10 ; i++) {
+			System.out.println(i+"단 출력? (Y/N)");
+			char cho = sc.next().charAt(0);
+			if (cho == 'N') {
+				break;
+			} else {
+				System.out.println("===== "+i+"단 =====");
+				for (int j=1 ; j<10 ; j++) {
+					System.out.println(i+"x"+j+" = "+i*j);
+				}
+			}
+			System.out.println();
+		}
+		
+		gugu:
+		for (int i=2 ; i<10 ; i++) {
+			for (int j=1 ; j<10 ; j++) {
+				if (j>5) {
+					break gugu;
+				}
+				System.out.println(i+"x"+j+" = "+i*j);
+			}
+		}
+	}
+	
+	public void practice() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("문자열을 입력하시오.");
+		String str = sc.next();
+		boolean flag = false;
+		stop:
+		for (int i=0 ; i<str.length() ; i++) {
+			for(int j=0 ; j<str.length() ; j++) {
+				if (i!=j && str.charAt(i)==str.charAt(j)){
+					flag = true;
+					System.out.println(str.charAt(i)+"중복!");
+					break stop;
+				}
+			}
+		}
+		System.out.println(flag? "" : "중복값 없음");
+	}
 }
